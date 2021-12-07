e5fdd44bb3ab7b55d5d0ae009f9f6949208412d9
Anton689 <zizugum@gmail.com>
2021-12-07 19:02:26 +0300

    update

---
+++ src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='header'>
+            <img
+                src='https://klike.net/uploads/posts/2020-04/1587719791_1.jpg'
+                alt=""/>
+        </header>
+    );
+}
+
+export default Header;
