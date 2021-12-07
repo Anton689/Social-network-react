74024b43e0ee7ac6a405c68dc73f7379d0c5a4c8
Anton689 <zizugum@gmail.com>
2021-12-07 19:01:49 +0300

    added project

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
