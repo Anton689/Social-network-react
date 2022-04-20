2de7240c0483005e8a29c59bfc8730cc2b1e13f4
Anton689 <zizugum@gmail.com>
2022-04-20 17:06:04 +0300

    train reselector lib.

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,4 +1,17 @@
 import {AppStateType} from './reduxStore';
+import {createSelector} from 'reselect';
+
+//пример использования селектора
+
+export const getUsers2 = (state: AppStateType) => {
+     return state.usersPage.users
+}
+
+export const getUsersSuperSelector = createSelector(getUsers2,(users)=> {
+    return users.filter(t=> true)
+})
+
+//----------------------------------------------------------------------------
 
 export const getUsers = (state: AppStateType) => {
      return state.usersPage
